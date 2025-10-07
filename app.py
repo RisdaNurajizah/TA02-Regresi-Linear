0aa552e9a0543253db5946d8ffa41a34a2194f75
RisdaNurajizah <risdanurazizah603@gmail.com>
2025-10-07 21:27:22 +0700

    update app.py

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 # Load model
 model = pickle.load(open('model.pkl', 'rb'))
 
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html') 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     form_values = []
